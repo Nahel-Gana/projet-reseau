7f8b4678a99b029e94c71a3ebc337a97dae24f5c
Nahel Gana <nahel.gana@etu.univ-lyon1.fr>
2026-04-08 11:02:32 +0200

    Finition ClientInfo

--- ClientInfo.java
+++ ClientInfo.java
@@ -4,34 +4,38 @@ public class ClientInfo {
     private int port ;
 
     public ClientInfo() {
-        
+        this.pseudo = "" ;
+        this.adresseIP = "" ;
+        this.port = 0 ;
     }
 
     public ClientInfo(String pseudo, String adresseIP, int port) {
-
+        this.pseudo = pseudo ;
+        this.adresseIP = adresseIP ;
+        this.port = port ;
     }
 
     public String getPseudo() {
-        return "" ;
+        return pseudo ;
     }
 
     public void setPseudo(String pseudo) {
-
+        this.pseudo = pseudo ;
     }
 
     public String getAdresseIP() {
-        return "" ;
+        return adresseIP ;
     }
 
     public void setAdresseIP(String adresseIP) {
-
+        this.adresseIP = adresseIP ;
     }
 
     public int getPort() {
-        return 0 ;
+        return port ;
     }
 
     public void setPort(int port) {
-        
+        this.port = port ;
     }
 }
