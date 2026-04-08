a4bbcc8dda2437c4cbbc19111514383557180e7d
Nahel Gana <nahel.gana@etu.univ-lyon1.fr>
2026-04-08 11:05:14 +0200

    Ajout commentaires ClientInfo

--- ClientInfo.java
+++ ClientInfo.java
@@ -1,41 +1,92 @@
+/**
+ * Classe représentant les informations d'un client.
+ * Elle contient le pseudo, l'adresse IP et le port du client.
+ */
 public class ClientInfo {
-    private String pseudo ;
-    private String adresseIP ;
-    private int port ;
+    
+    /** Pseudo du client */
+    private String pseudo;
+    
+    /** Adresse IP du client */
+    private String adresseIP;
+    
+    /** Port utilisé par le client */
+    private int port;
 
+    /**
+     * Constructeur par défaut.
+     * Initialise les attributs avec des valeurs vides ou nulles.
+     */
     public ClientInfo() {
-        this.pseudo = "" ;
-        this.adresseIP = "" ;
-        this.port = 0 ;
+        this.pseudo = "";
+        this.adresseIP = "";
+        this.port = 0;
     }
 
+    /**
+     * Constructeur avec paramètres.
+     *
+     * @param pseudo le pseudo du client
+     * @param adresseIP l'adresse IP du client
+     * @param port le port utilisé par le client
+     */
     public ClientInfo(String pseudo, String adresseIP, int port) {
-        this.pseudo = pseudo ;
-        this.adresseIP = adresseIP ;
-        this.port = port ;
+        this.pseudo = pseudo;
+        this.adresseIP = adresseIP;
+        this.port = port;
     }
 
+    /**
+     * Retourne le pseudo du client.
+     *
+     * @return le pseudo
+     */
     public String getPseudo() {
-        return pseudo ;
+        return pseudo;
     }
 
+    /**
+     * Modifie le pseudo du client.
+     *
+     * @param pseudo le nouveau pseudo
+     */
     public void setPseudo(String pseudo) {
-        this.pseudo = pseudo ;
+        this.pseudo = pseudo;
     }
 
+    /**
+     * Retourne l'adresse IP du client.
+     *
+     * @return l'adresse IP
+     */
     public String getAdresseIP() {
-        return adresseIP ;
+        return adresseIP;
     }
 
+    /**
+     * Modifie l'adresse IP du client.
+     *
+     * @param adresseIP la nouvelle adresse IP
+     */
     public void setAdresseIP(String adresseIP) {
-        this.adresseIP = adresseIP ;
+        this.adresseIP = adresseIP;
     }
 
+    /**
+     * Retourne le port du client.
+     *
+     * @return le port
+     */
     public int getPort() {
-        return port ;
+        return port;
     }
 
+    /**
+     * Modifie le port du client.
+     *
+     * @param port le nouveau port
+     */
     public void setPort(int port) {
-        this.port = port ;
+        this.port = port;
     }
 }
